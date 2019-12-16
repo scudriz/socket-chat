abdc9e1c6cac4eebe29e475a1bc15695e5539cb1
scudriz <samue0109@gmail.com>
2019-12-15 20:37:52 -0500

    Se agrega el front

--- public/js/socket-chat.js
+++ public/js/socket-chat.js
@@ -4,23 +4,25 @@ var params = new URLSearchParams(window.location.search);
 
 if (!params.has('nombre') || !params.has('sala')) {
     window.location = 'index.html';
-    throw new Error('El nombre/sala es necesario');
+    throw new Error('El nombre y sala son necesarios');
 }
 
 var usuario = {
     nombre: params.get('nombre'),
     sala: params.get('sala')
-}
+};
+
+
 
 socket.on('connect', function() {
     console.log('Conectado al servidor');
+
     socket.emit('entrarChat', usuario, function(resp) {
-        console.log(resp);
+        console.log('Usuarios conectados', resp);
+        renderizarUsuarios(resp);
     });
 
-
 });
-
 
 // escuchar
 socket.on('disconnect', function() {
@@ -30,27 +32,31 @@ socket.on('disconnect', function() {
 });
 
 
+// Enviar información
+// socket.emit('crearMensaje', {
+//     nombre: 'Fernando',
+//     mensaje: 'Hola Mundo'
+// }, function(resp) {
+//     console.log('respuesta server: ', resp);
+// });
+
 // Escuchar información
 socket.on('crearMensaje', function(mensaje) {
-
     console.log('Servidor:', mensaje);
-
+    renderizarMensajes(mensaje, false);
+    scrollBottom();
 });
 
-//socket.emit('crearMensaje', {
-//    mensaje: 'El mensaje que quiero enviar'
-//}, (resp) => {
-//    console.log(resp);
-//});
-
-
-// Escuchar la lista de todos los usarios si existe evento de conexión o desconexión
-socket.on('listaPersonas', function(mensaje) {
-    console.log(mensaje);
+// Escuchar cambios de usuarios
+// cuando un usuario entra o sale del chat
+socket.on('listaPersonas', function(personas) {
+    console.log(personas);
+    renderizarUsuarios(personas);
 });
 
-// mensajes privados.abs
-
+// Mensajes privados
 socket.on('mensajePrivado', function(mensaje) {
-    console.log('Mensaje Privado: ', mensaje);
+
+    console.log('Mensaje Privado:', mensaje);
+
 });
--- server/sockets/socket.js
+++ server/sockets/socket.js
@@ -19,6 +19,7 @@ io.on('connection', (client) => {
         usuarios.agregarPersona(client.id, data.nombre, data.sala);
 
         client.broadcast.to(data.sala).emit('listaPersonas', usuarios.getPersonasPorSala(data.sala));
+        client.broadcast.to(data.sala).emit('crearMensaje', crearMensaje('Administrador', `${ data.nombre } se unió al chat`));
 
         callback(usuarios.getPersonasPorSala(data.sala));
     })
@@ -29,11 +30,15 @@ io.on('connection', (client) => {
         client.broadcast.to(personaBorrada.sala).emit('listaPersonas', usuarios.getPersonasPorSala(personaBorrada.sala));
     });
 
-    client.on('crearMensaje', (data) => {
+    client.on('crearMensaje', (data, callback) => {
         let persona = usuarios.getPersona(client.id);
+
         let mensaje = crearMensaje(persona.nombre, data.mensaje);
         client.broadcast.to(persona.sala).emit('crearMensaje', mensaje);
+
+        callback(mensaje);
     });
+
 
     client.on('mensajePrivado', data => {
         let persona = usuarios.getPersona(client.id);
